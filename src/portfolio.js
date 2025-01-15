537e7ddb8f9f3ed6a3ab90565931e29dde2a0364
Divya Vispute <divyavispute86@gmail.com>
2025-01-15 22:12:39 +0530

    Initialize

---
+++ src/portfolio.js
@@ -0,0 +1,183 @@
+/* Change this file to get your personal Portfolio */
+
+// To change portfolio colors globally go to the  _globalColor.scss file
+
+import emoji from "react-easy-emoji";
+import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+
+// Splash Screen
+
+const splashScreen = {
+  enabled: true, // set false to disable splash screen
+  animation: splashAnimation,
+  duration: 2000 // Set animation duration as per your animation
+};
+
+// Summary And Greeting Section
+
+const illustration = {
+  animated: true // Set to false to use static SVG
+};
+
+const greeting = {
+  username: "Divya Vispute",
+  title: "Hi all, I'm Divya",
+  subTitle: emoji(
+    "A passionate Frontend Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks."
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/divyavispute86",
+  linkedin: "https://linkedin.com/in/divya-vispute-785a49168",
+  gmail: "divyavispute86@gmail.com",
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY FROTEND DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+    ),
+    emoji("⚡ Created business related articles/pages using CMS/ECM as per client requirements")
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "fab fa-sass"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "bootstrap",
+      fontAwesomeClassname: "fab fa-bootstrap"
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      progressPercentage: "90%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "60%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Senior Anaylst",
+      company: "eClerx",
+      companylogo: require("./assets/images/eClerxLogo.jpeg"),
+      date: "Nov 2022 – Present",
+      descBullets: [
+        "Optimized HTML, CSS, and React components to increase web page by 100%, resulting in improved user experience and boosted website traffic.",
+        "Experience in analysis and transformation of business requirements into functional and non-functional flows.",
+        "Maintaining process in Kanban and carryout quality assurance tests pages, layouts of the site."
+      ]
+    },
+    {
+      role: "Senior Software Enginner",
+      company: "Adapty Solutions Pvt. Ltd.",
+      companylogo: require("./assets/images/adaptyLogo.jpeg"),
+      date: "Jan 2020 – Nov 2022",
+      descBullets: [
+        "Developed the Front-End and User Interface and responsible for designing page layout.",
+        "Created reusable React components and managed application state with Redux.",
+        "Created business related pages using CMS for complex design projects for corporate clients.",
+        "Maintaining process in JIRA and carryout quality assurance tests pages, layouts of the site."
+      ]
+    }
+  ]
+};
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Resume Section
+
+const resumeSection = {
+  title: "Resume",
+  subtitle: "Feel free to download my resume",
+
+  // Please Provide with Your Podcast embeded Link
+  display: true // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Discuss a project or just want to say hi? My Inbox is open for all.",
+  number: "+92-8652550796",
+  email_address: "divyavispute86@gmail.com"
+};
+ 
+
+const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  splashScreen,
+  skillsSection,
+  techStack,
+  workExperiences,
+  openSource,
+  contactInfo,
+  isHireable,
+  resumeSection
+};
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+    assetModuleFilename: 'assets/[name][ext][query]', // Output directory for assets
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i, // Image files
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|)$/, // Font files
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/, // CSV and TSV files
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/, // XML files
+        use: ['xml-loader'],
+      },
+      {
+        test: /\.(pdf)$/i, // Match PDF files
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    historyApiFallback: true, // Handle client-side routing
+  },
+  mode: 'development',
+};
